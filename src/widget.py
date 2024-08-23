92c646b22f4a7caa89dddbb0962bb0b35fd8d11f
Anastasiya Nikolaeva <eritreya666@gmail.com>
2024-08-23 15:06:49 +0300

    добавлен widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,5 @@
-from typing import Union
 from datetime import datetime
+from typing import Union
 
 from src.masks import get_mask_account, get_mask_card_number
 
@@ -15,9 +15,8 @@ def mask_account_card(user_number: Union[str]) -> str:
     return result
 
 
-# def get_date(user_date: Union[str]) -> str:
-#     """Функция получения даты в определенном формате и возвращения в формате ДД.ММ.ГГГГ"""
-#     date_format = datetime.strptime(user_date, '%y-%m-%dT%H:%M:%S:%f')
-#     new_date = date_format.strptime('%d-%m-%y')
-#     return new_date
-# print(get_date(2024-03-11T02:26:18.671407))
+def get_date(user_date: Union[str]) -> str:
+    """Функция получения даты в определенном формате и возвращения в формате ДД.ММ.ГГГГ"""
+    date_format = datetime.strptime(user_date, "%Y-%m-%dT%H:%M:%S.%f")
+    new_date = date_format.strftime("%d.%m.%Y")
+    return new_date
