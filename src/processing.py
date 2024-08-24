ed3d970505f2deb6fccfa0d203c677db3174c28b
Anastasiya Nikolaeva <eritreya666@gmail.com>
2024-08-25 01:47:45 +0300

    add two functions processing.py

--- src/processing.py
+++ src/processing.py
@@ -12,6 +12,13 @@ def filter_by_state(dictionaries: list, state: str = "EXECUTED") -> list:
     return new_list
 
 
+def sort_by_date(dictionaries: list) -> list:
+    """Функция сортирующая список по определенному ключу"""
+
+    sorted_list = sorted(dictionaries, key=lambda dictionaries: dictionaries["date"], reverse=True)
+    return sorted_list
+
+
 if __name__ == "__main__":
     filtered = filter_by_state(
         [
@@ -23,4 +30,17 @@ if __name__ == "__main__":
     )
 
     for i in filtered:
+        print(i)
+
+
+if __name__ == "__main__":
+    ordered = sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+    for i in ordered:
         print(i)
