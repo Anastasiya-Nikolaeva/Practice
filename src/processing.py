e9a03305a1d3f2eebd68e2673d0a5b538ce5064f
Anastasiya Nikolaeva <eritreya666@gmail.com>
2024-08-29 21:13:26 +0300

    add ubdate

---
+++ src/processing.py
@@ -0,0 +1,29 @@
+from operator import itemgetter
+from typing import Any, Iterable, Union
+
+
+def filter_by_state(dictionaries: Iterable[dict], state: Union[str] = "EXECUTED") -> Any:
+    """Функция из списка словарей возвращает новый список словарей,
+    у которых ключ state соответствует указанному значению"""
+    new_list = []
+    alternative_list = []
+
+    for key in dictionaries:
+        if key.get("state") == state:
+            new_list.append(key)
+        elif key.get("state") == "CANCELED":
+            alternative_list.append(key)
+    if len(new_list) == 0:
+        return "В списке нет необходимых элементов"
+    return new_list
+
+
+def sort_by_date(dictionaries: Any, sort_revers: bool = True) -> Any:
+    """Функция сортирующая список по определенному ключу"""
+    if dictionaries != [{}]:
+        return sorted(dictionaries, key=itemgetter("date"), reverse=sort_revers)
+    return "В списке нет элементов"
+#
+#
+# with pytest.raises(KeyError):
+#     sort_by_date(KeyError)
--- src/widget.py
+++ src/widget.py
@@ -1,21 +1,29 @@
 from datetime import datetime
-from typing import Union
+from typing import Any, Union
 
-from src.masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_card_number
 
 
 def mask_account_card(user_number: Union[str]) -> str:
     """Функция маскировки карт или счетов"""
-    if len(user_number.split()[-1]) == 20:
-        new_number = get_mask_account(user_number)
-        result = user_number[:-20] + new_number
-    elif len(user_number.split()[-1]) == 16:
-        new_number = get_mask_card_number(user_number.split()[-1])
-        result = user_number[:-16] + new_number
-    return result
+    if user_number != "":
+        new_number = user_number.replace(" ", "")
+        if new_number[-20:].isdigit():
+            if new_number[-20:].isdigit() and len(new_number) == 24:
+                mask_num = ["**"]
+                mask_num += new_number[-4:]
+                mask_account_user = "".join(mask_num)
+                return new_number[:4] + " " + mask_account_user
+            return "Неверный ввод данных"
+        elif len(user_number.split()[-1]) == 16:
+            new_number = get_mask_card_number(user_number.split()[-1])
+            result = user_number[:-16] + new_number
+            return result
+        return "Неверный ввод данных"
+    return "Вы ввели пустой запрос"
 
 
-def get_date(user_date: Union[str]) -> str:
+def get_date(user_date: Union[str]) -> Any:
     """Функция получения даты в определенном формате и возвращения в формате ДД.ММ.ГГГГ"""
     date_format = datetime.strptime(user_date, "%Y-%m-%dT%H:%M:%S.%f")
     new_date = date_format.strftime("%d.%m.%Y")
